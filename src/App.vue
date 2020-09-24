cdc9d7fd489ef8479543de53bbfc46ae5132b995
吕达钊 <1240730742@qq.com>
2020-09-24 23:41:21 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div id="app">
     <router-view></router-view>
     <main-tab-bar></main-tab-bar>
@@ -19,30 +18,4 @@
 
 <style>
 	@import url("assets/css/base.css");
-=======
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
->>>>>>> 57b1ea100a4f1b303727d50cb4bb1a4276fa7088
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
 import router from './router'
 
 createApp(App).use(router).mount('#app')
-=======
-
-createApp(App).mount('#app')
->>>>>>> 57b1ea100a4f1b303727d50cb4bb1a4276fa7088
