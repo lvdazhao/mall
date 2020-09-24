6314c7ea0dbbe92b99ba8ef591cdac8fe9248f48
吕达钊 <1240730742@qq.com>
2020-09-24 23:20:12 +0800

    完成tabbar

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<tab-bar>
+		<tab-bar-item path="/home" activeColor="purple">
+      <template v-slot:item-icon><img src="@/assets/img/tabbar/home.svg" alt=""></template>
+      <template v-slot:item-icon-active><img src="@/assets/img/tabbar/home_active.svg" alt=""></template>
+      <template v-slot:item-text><div >首页</div></template>
+		</tab-bar-item>
+		<tab-bar-item path="/category" activeColor="purple">
+			<template v-slot:item-icon><img src="@/assets/img/tabbar/category.svg" alt=""></template>
+			<template v-slot:item-icon-active><img src="@/assets/img/tabbar/category_active.svg" alt=""></template>
+			<template v-slot:item-text><div>分类</div></template>
+		</tab-bar-item>
+		<tab-bar-item path="/cart" activeColor="purple">
+			<template v-slot:item-icon><img src="@/assets/img/tabbar/shopcart.svg" alt=""></template>
+			<template v-slot:item-icon-active><img src="@/assets/img/tabbar/shopcart_active.svg" alt=""></template>
+			<template v-slot:item-text><div>购物车</div></template>
+		</tab-bar-item>
+		<tab-bar-item path="/profile" activeColor="purple">
+			<template v-slot:item-icon><img src="@/assets/img/tabbar/profile.svg" alt=""></template>
+			<template v-slot:item-icon-active><img src="@/assets/img/tabbar/profile_active.svg" alt=""></template>
+			<template v-slot:item-text><div>我的</div></template>
+		</tab-bar-item>
+	</tab-bar>
+</template>
+
+<script>
+	import TabBar from "@/components/common/tabbar/tabBar.vue"
+	import TabBarItem from "@/components//common/tabbar/TabBarItem.vue"
+
+	export default {
+		name: "MainTabBar",
+		components: {
+			TabBar,
+			TabBarItem
+		}
+	}
+</script>
+
+<style>
+</style>
